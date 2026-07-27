2dc14e8a7ca39f5270ebee55a4b85788607c77b7
Shivam Mishra <scm.mymail@gmail.com>
2026-07-27 16:36:12 +0530

    feat: build captain knowledge maps from approved FAQs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_18_000000) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_27_120001) do
   # These extensions should be enabled to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -376,6 +376,9 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_18_000000) do
     t.jsonb "config", default: {}, null: false
     t.jsonb "response_guidelines", default: []
     t.jsonb "guardrails", default: []
+    t.jsonb "knowledge_map", default: {}, null: false
+    t.string "knowledge_map_source_digest"
+    t.datetime "knowledge_map_built_at"
     t.index ["account_id"], name: "index_captain_assistants_on_account_id"
   end
 
